5fa6798753c7231f86f885f77ba4a07a2e4fa30b
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-17 13:46:00 +0300

    fun

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var number = function(num) {
+var chot = 0, nechot = 0;
+    for (var i = 0; i < num.length; i++) {
+        if(num[i] % 2 == 0) {
+            chot++
+        } else {
+            nechot++
+        }
+    }
+    return[chot, nechot];
+}
